894c478accbbaf46bf0eea0e425a8af075af35d1
Cesar Kawakami <cesarkawakami@gmail.com>
2015-08-19 11:23:50 -0300

    initial test

---
+++ linker.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         AWS Linker
+// @namespace    https://github.com/cesarkawakami/aws-linker/
+// @version      0.1
+// @description  Improvements for AWS Console interface
+// @author       Cesar Kawakami
+// @match        https://*console.aws.amazon.com/*
+// @grant        none
+// ==/UserScript==
+
+console.log("I'm working!");
